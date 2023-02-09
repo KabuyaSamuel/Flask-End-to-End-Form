e96047b0b7543859da3ffc9bfcc99404a4b2cdaf
Samuel Kabuya <samuel.mwangikabuya@kibo.school>
2023-02-09 22:52:09 +0300

    Establishing db connection

--- app.py
+++ app.py
@@ -24,27 +24,3 @@ def show_form():
 
 
 # Route handler for submitting the form
-@app.route("/submit", methods=["POST"])
-def submit_form():
-    # Get the form data
-    name = request.form["name"]
-    email = request.form["email"]
-    message = request.form["message"]
-
-    # Save the form data to the database
-    cursor.execute("""INSERT INTO form_data (name, email, message)
-                      VALUES (?, ?, ?)""", (name, email, message))
-    conn.commit()
-
-    # Redirect to the list of items
-    return redirect(url_for("list_items"))
-
-# Route handler to display all items
-@app.route("/items")
-def list_items():
-    cursor.execute("SELECT * FROM form_data")
-    items = cursor.fetchall()
-    return render_template("items.html", items=items)
-
-if __name__ == "__main__":
-    app.run(debug=True)
